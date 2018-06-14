6f1cc44f3cd245fa845d9f61d41a65e0c6b26fd5
Andrei <taysan003@gmail.com>
2018-06-14 00:19:51 -0700

    csv file

--- src/main/java/com/express/base/CsvDataProvider.java
+++ src/main/java/com/express/base/CsvDataProvider.java
@@ -1,7 +1,7 @@
 package com.express.base;
+/*import au.com.bytecode.opencsv.CSVReader;*/
 
-import au.com.bytecode.opencsv.CSVReader;
-import com.opencsv.CSVReader;
+import com.opencsv.*;
 import org.testng.annotations.DataProvider;
 
 import java.io.File;
@@ -13,33 +13,33 @@ import java.util.*;
 
 public class CsvDataProvider {
 
-    @DataProvider (name = "CsvDataProvider")
-    public static Iterator <Object[]> provideData(Method method){
-        List<Object[]> list = new ArrayList<Object[]>();
-        String paphname = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "test_data"
+    @DataProvider(name = "CsvDataProvider")
+    public static Iterator<Object[]> provideData(Method method) {
+        List<Object[]> list = new ArrayList<>();
+        String pathname = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "test_data"
                 + File.separator + method.getDeclaringClass().getSimpleName() + "_" + method.getName() + ".csv";
-        File file  = new File(paphname);
+        File file = new File(pathname);
         try {
             CSVReader reader = new CSVReader(new FileReader(file));
             String[] keys = reader.readNext();
-            if (keys !=null) {
+            if (keys != null) {
                 String[] dataParts;
-                while ((dataParts = reader.readNext()) !=null);
-                Map<String, String> testData = new HashMap<String, String>();
-                for (int i = 0; i <keys.length ; i++) {
-                    testData.put(keys[i], dataParts[i]);
+                while ((dataParts = reader.readNext()) != null) ;
+                {
+                    Map<String, String> testData = new HashMap<>();
+                    for (int i = 0; i < keys.length; i++) {
+                        testData.put(keys[i], dataParts[i]);
+                    }
+                    list.add(new Object[]{testData});
                 }
-                list.add(new Object[]{testData});
             }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File " + pathname + "was not found.\n" + e.getStackTrace().toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read " + pathname + "file.\n" + e.getStackTrace().toString());
         }
-        reader.close();
-
-    }catch (FileNotFoundException e) {
-        throw new RuntimeException("File " + pathname + "was not found.\n" + e.getStackTrace().toString());
-    } catch (IOException e) {
-        throw RuntimeException("Could not read " + pathname + "file.\n" + e.getStackTrace().toString());
-
+        return list.iterator();
     }
-
 
 }
